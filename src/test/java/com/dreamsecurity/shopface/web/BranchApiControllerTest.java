7cbf8e75dc525edd5580e181c3101bf63b50a5b9
gmu1026 <gmu01026@gmail.com>
2020-08-07 10:56:31 +0900

    Fix: Gradle Config

--- src/test/java/com/dreamsecurity/shopface/web/BranchApiControllerTest.java
+++ src/test/java/com/dreamsecurity/shopface/web/BranchApiControllerTest.java
@@ -15,6 +15,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.JUnitRestDocumentation;
