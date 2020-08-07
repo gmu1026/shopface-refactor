f7a934ebc970c073604447ed62135894642cb1be
gmu1026 <gmu01026@gmail.com>
2020-08-07 17:49:13 +0900

    Fix: Response Code

--- src/main/java/com/dreamsecurity/shopface/response/ApiResponseCode.java
+++ src/main/java/com/dreamsecurity/shopface/response/ApiResponseCode.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum ApiResponseCode {
     OK("요청이 성공했습니다"),
     BAD_REQUEST("요청 파라미터가 잘못되었습니다"),
-    NOT_FOUND("요청 API가 업습니다"),
+    PAGE_NOT_FOUND("요청 API가 업습니다"),
+    NOT_FOUND("해당 자원이 없습니다"),
     UNAUTHORIZED("인증 토큰이 없습니다"),
     SERVER_ERROR("서버 에러입니다");
 
