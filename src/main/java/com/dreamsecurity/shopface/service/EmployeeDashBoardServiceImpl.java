c639a6fe9e157c46552891e172a54462aa3343c5
gmu1026 <gmu01026@gmail.com>
2020-08-25 18:58:19 +0900

    fix: employee dashboard salary, hours error

--- src/main/java/com/dreamsecurity/shopface/service/EmployeeDashBoardServiceImpl.java
+++ src/main/java/com/dreamsecurity/shopface/service/EmployeeDashBoardServiceImpl.java
@@ -19,8 +19,8 @@ public class EmployeeDashBoardServiceImpl implements EmployeeDashBoardService{
         List<EmployeeDashBoardListResponseDto> responseDtos = employeeDashBoardRepository.getEmployeeDashBoardListScheduled(memberId);
 
         for (EmployeeDashBoardListResponseDto responseDto : responseDtos) {
-            long hoursPlan = ChronoUnit.MINUTES.between(responseDto.getWorkEndTime(), responseDto.getWorkStartTime());
-            responseDto.setHoursPlan((hoursPlan / 60) + ((hoursPlan % 60) * 0.01));
+            long hoursPlan = ChronoUnit.MINUTES.between(responseDto.getWorkStartTime(), responseDto.getWorkEndTime());
+            responseDto.setHoursPlan((double) hoursPlan / 60);
             responseDto.setSalaryPlan(responseDto.getEmploySalary() * responseDto.getHoursPlan());
         }
         return responseDtos;
@@ -31,22 +31,22 @@ public class EmployeeDashBoardServiceImpl implements EmployeeDashBoardService{
         List<EmployeeDashBoardListResponseDto> responseDtos = employeeDashBoardRepository.getEmployeeDashBoardListWorkDone(memberId);
 
         for (EmployeeDashBoardListResponseDto responseDto : responseDtos) {
-            long hoursPlan = ChronoUnit.MINUTES.between(responseDto.getWorkEndTime(), responseDto.getWorkStartTime());
-            responseDto.setHoursPlan((hoursPlan / 60) + ((hoursPlan % 60) * 0.01));
+            long hoursPlan = ChronoUnit.MINUTES.between(responseDto.getWorkStartTime(), responseDto.getWorkEndTime());
+            responseDto.setHoursPlan((double) hoursPlan / 60);
             responseDto.setSalaryPlan(responseDto.getEmploySalary() * responseDto.getHoursPlan());
 
             //출근처리했지만 퇴근처리는 안하고 스케줄이 끝난경우
             if (responseDto.getWorkingTime() != null && responseDto.getQuittingTime() == null) {
-                long actualHours = ChronoUnit.MINUTES.between(responseDto.getWorkEndTime(), responseDto.getWorkingTime());
-                responseDto.setActualWorkingHours((actualHours / 60) + ((actualHours % 60) * 0.01));
+                long actualHours = ChronoUnit.MINUTES.between(responseDto.getWorkingTime(), responseDto.getWorkEndTime());
+                responseDto.setActualWorkingHours((double) actualHours / 60);
                 responseDto.setActualSalary(responseDto.getEmploySalary() * responseDto.getActualWorkingHours());
             } else if (responseDto.getWorkingTime() == null && responseDto.getQuittingTime() != null) { // 출근처리는 안햇지만 퇴근처리만 한 경우
-                long actualHours = ChronoUnit.MINUTES.between(responseDto.getQuittingTime(), responseDto.getWorkStartTime());
-                responseDto.setActualWorkingHours((actualHours / 60) + ((actualHours % 60) * 0.01));
+                long actualHours = ChronoUnit.MINUTES.between(responseDto.getWorkStartTime(), responseDto.getQuittingTime());
+                responseDto.setActualWorkingHours((double) actualHours / 60);
                 responseDto.setActualSalary(responseDto.getEmploySalary() * responseDto.getActualWorkingHours());
             } else {// 출근처리와 퇴근처리를 정상적으로 한 경우
-                long actualHours = ChronoUnit.MINUTES.between(responseDto.getQuittingTime(), responseDto.getWorkingTime());
-                responseDto.setActualWorkingHours((actualHours / 60) + ((actualHours % 60) * 0.01));
+                long actualHours = ChronoUnit.MINUTES.between(responseDto.getWorkingTime(), responseDto.getQuittingTime());
+                responseDto.setActualWorkingHours((double) actualHours / 60);
                 responseDto.setActualSalary(responseDto.getEmploySalary() * responseDto.getActualWorkingHours());
             }
         }
