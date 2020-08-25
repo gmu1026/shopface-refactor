2a35565f6ccc5a5a577c78f3f14c5406df65f600
gmu1026 <gmu01026@gmail.com>
2020-08-25 18:57:35 +0900

    resource: del unused exception class

--- src/main/java/com/dreamsecurity/shopface/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.dreamsecurity.shopface.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException() {
-        super("요청한 자원을 찾지 못했습니다");
-    }
-}
